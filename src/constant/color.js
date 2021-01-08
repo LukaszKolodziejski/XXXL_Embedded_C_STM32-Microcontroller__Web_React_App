9c9c6d05061147ed3de2c924009085a120bfdb16
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-01-08 16:16:08 +0100

    change to constant

---
+++ src/constant/color.js
@@ -0,0 +1,4 @@
+export const broun = "rgba(100, 85, 85,1)";
+export const green = "rgba(26, 223, 56, 0.564)";
+export const blue = "rgba(24, 16, 250, 0.564)";
+export const red = "rgba(235, 60, 6, 0.564)";
